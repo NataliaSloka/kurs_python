a6563dbba5b91caaaa3e5c6627a2255fe7cb02ff
Natalia <kingajg@gmail.com>
2019-12-01 16:52:46 +0100

    wysłane01122019

--- plot.py
+++ plot.py
@@ -1,5 +1,20 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
-print(df)
+df['E'] =df['A'] + df['B']
+a = df['E'].sum()
+print(df[['A', 'B', 'E']])
+
+#df.to_csv('tablica.csv')
+df1 = pd.read_csv('tablica.csv')
+print(df1)
+
+plt.plot(df['A'], df['B'], c='y', label='Pierwszy wykres', marker='^')
+plt.ylim(0,100)
+plt.xlim(0,100)
+plt.xlabel('kolumna A [\u03bcm]')
+plt.ylabel('kolumna B [\u03bcm]')
+plt.legend()
+plt.show()
